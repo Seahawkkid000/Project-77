c9a48f955ea1f0cb667999c05e365141ff3cb6bb
Seahawkkid000 <107086227+Seahawkkid000@users.noreply.github.com>
2022-11-15 18:56:51 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+var a = 10678
+
+console.log(a)
+
+var b = 47692
+
+console.log(b)
+
+var First_Name = Luka
+
+console.log(First_Name)
+
+var Second_Name = Morant
+
+console.log(Second_Name)
+
+var Complete_Name = First_Name.concat(Second_Name)
+
+console.log(Complete_Name)
+
+ var Add = a + b
+
+ console.log(Add)
+
+ var Subtract = a - b
+
+ console.log(Subtract)
+
+ var Multiply = a.Multiply(b)
+
+ console.log(Multiply)
+
+ var Divide = a.Divide(b)
